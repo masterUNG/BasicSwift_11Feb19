961886713020fef41e6fb153d4f50374fc47a656
masterUNG <phrombutr@gmail.com>
2019-02-11 10:03:18 +0700

    Variable

---
+++ Variable.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+//Variable and Constant
+//Implicit Variable คือ การประกาศที่ กำหนด DataType โดยใช้ Value กำหนด
+var name1 = "Doramon"   // String Type
+var number1 = 123       // Integer Type
+var number2 = 3.1416    // Double Type
+var status1 = true      // Bool Type
+
+//Explcit Variable การประกาศ ที่ กำหนด DataType เองเลย
+var name2: String = "Nopita"
+var number3: Int = 1234
+var number4: Double = 123.45
+var status2: Bool = false
+
+
+
+
+
+
+
+
