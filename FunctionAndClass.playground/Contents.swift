e34e2c677c220a5f769a60a4afcc0719bc5a97fa
masterUNG <phrombutr@gmail.com>
2019-02-11 13:50:42 +0700

    Field OK

---
+++ FunctionAndClass.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+class MyClass {
+    
+    var myName: String = "Doramon"
+    var amount: Int = 2
+    
+    
+    
+    
+}   // MyClass
+
+//Inheiate Class
+var myClass = MyClass()
+
+//Show field and Assign field
+print(myClass.myName)
+myClass.myName = "Nopita"
+print(myClass.myName)
+
+
+
+
