ea4ad412a6e4b79d8813191c6f1632bee8fbd05a
masterUNG <phrombutr@gmail.com>
2019-02-11 10:13:34 +0700

    Operate

--- Variable.playground/Contents.swift
+++ Variable.playground/Contents.swift
@@ -13,6 +13,18 @@ var number3: Int = 1234
 var number4: Double = 123.45
 var status2: Bool = false
 
+//Constant
+let number5: Int = 1000
+
+number3 = 5000
+//number5 = 5000  // Error
+
+//Operate
+var answer = number1 + 1000 + Int(number2)
+var answer2 = number2 + 0.5 + Double(number1)
+
+
+
 
 
 
